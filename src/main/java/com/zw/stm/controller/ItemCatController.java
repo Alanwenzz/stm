5e2c003347b8ddb78f6570fef16bb61e8d26d73c
周闻 <364946726@qq.com>
2019-08-13 09:48:30 +0800

    分页显示(pagehelper)  选择类目

---
+++ src/main/java/com/zw/stm/controller/ItemCatController.java
@@ -0,0 +1,31 @@
+package com.zw.stm.controller;
+
+import com.zw.stm.common.EasyUITreeNode;
+import com.zw.stm.pojo.TbItemCat;
+import com.zw.stm.service.ItemCatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class ItemCatController {
+    private final ItemCatService itemCatService;
+
+    @Autowired
+    public ItemCatController(ItemCatService itemCatService) {
+        this.itemCatService = itemCatService;
+    }
+
+    @RequestMapping("/item/cat/list")
+    @ResponseBody
+    public List<EasyUITreeNode> getItemCatList(@RequestParam(value="id",defaultValue="0") Long parentId){
+        //1.引入服务
+        //2.注入服务
+        //3.调用方法
+        return itemCatService.getItemCatListByParentId(parentId);
+    }
+}
